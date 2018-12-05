1070072c66989a93e61f20947c63b1688413b173
zhangjie <731255090@qq.com>
2018-12-05 09:58:17 +0800

    并发案例整理

---
+++ src/main/java/com/example/concurrency/ConcurrencyTest.java
@@ -0,0 +1,49 @@
+package com.example.concurrency;
+
+import com.example.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class ConcurrencyTest {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //threadTotal:同一时间并发的线程数
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        //所有请求完后开始统计请求结果
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal ; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire(); //判断当前线程是否允许被执行
+                    add();
+                    semaphore.release();//释放当前线程
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count);
+    }
+
+    private static void add() {
+        count++;
+    }
+}
---
+++ src/main/java/com/example/concurrency/example/singleton/Singleton.java
@@ -0,0 +1,5 @@
+package com.example.concurrency.example.singleton;
+
+public enum Singleton {
+    INSTANCE;
+}
